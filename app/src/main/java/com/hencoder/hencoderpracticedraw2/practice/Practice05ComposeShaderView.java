c8a0bb3e88d7647216b05331722f86d0dae10263
yao.cui <yao.cui@weimob.com>
2018-01-04 17:55:52 +0800

    作业完成提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
@@ -1,11 +1,20 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.ComposeShader;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Shader;
+import android.graphics.Xfermode;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
 
 public class Practice05ComposeShaderView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -28,6 +37,12 @@ public class Practice05ComposeShaderView extends View {
         // 用 Paint.setShader(shader) 设置一个 ComposeShader
         // Shader 1: BitmapShader 图片：R.drawable.batman
         // Shader 2: BitmapShader 图片：R.drawable.batman_logo
+        Shader shader = new BitmapShader(BitmapFactory.decodeResource(getResources(), R.drawable.batman),
+                Shader.TileMode.CLAMP,Shader.TileMode.CLAMP);
+        Shader shader1 = new BitmapShader(BitmapFactory.decodeResource(getResources(),R.drawable.batman_logo),
+                Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Shader comShader = new ComposeShader(shader,shader1, PorterDuff.Mode.DST_IN);
+        paint.setShader(comShader);
     }
 
     @Override
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice12PathEffectView.java
@@ -2,8 +2,15 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.SumPathEffect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -42,35 +49,56 @@ public class Practice12PathEffectView extends View {
         // 使用 Paint.setPathEffect() 来设置不同的 PathEffect
 
         // 第一处：CornerPathEffect
+        PathEffect pathEffect = new CornerPathEffect(20);
+        paint.setPathEffect(pathEffect);
         canvas.drawPath(path, paint);
 
         canvas.save();
         canvas.translate(500, 0);
         // 第二处：DiscretePathEffect
+        PathEffect pathEffect1 = new DiscretePathEffect(20,5);
+        paint.setPathEffect(pathEffect1);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 200);
         // 第三处：DashPathEffect
+        PathEffect pathEffect2 = new DashPathEffect(new float[]{20,5,10,5},0);
+        paint.setPathEffect(pathEffect2);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 200);
         // 第四处：PathDashPathEffect
+        Path shape = new Path();
+        shape.lineTo(40,0);
+        shape.lineTo(20,20);
+        shape.close();
+        PathEffect pathEffect3 = new PathDashPathEffect(shape,50,0,
+                PathDashPathEffect.Style.TRANSLATE);
+        paint.setPathEffect(pathEffect3);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(0, 400);
         // 第五处：SumPathEffect
+        PathEffect pathEffect4 = new DashPathEffect(new float[]{10,5,10,5},0);
+        PathEffect pathEffect5 = new DiscretePathEffect(20,5);
+        PathEffect pathEffect6 = new SumPathEffect(pathEffect4,pathEffect5);
+        paint.setPathEffect(pathEffect6);
         canvas.drawPath(path, paint);
         canvas.restore();
 
         canvas.save();
         canvas.translate(500, 400);
         // 第六处：ComposePathEffect
+        PathEffect pathEffect7 = new DashPathEffect(new float[]{10,5,20,5},0);
+        PathEffect pathEffect8 = new DiscretePathEffect(20,5);
+        PathEffect pathEffect9 = new ComposePathEffect(pathEffect7,pathEffect8);
+        paint.setPathEffect(pathEffect9);
         canvas.drawPath(path, paint);
         canvas.restore();
     }
